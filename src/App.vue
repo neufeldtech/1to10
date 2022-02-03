1248c945b2980b44dcf4ba57a1e6af0fe3bcaac3
Jordan Neufeld <jordan@mbp16.local>
2022-02-03 12:13:34 -0600

    Rewrite in vue

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
+      <v-toolbar-title>One to Ten</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon @click.stop="newGame">mdi-autorenew</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld drawerState="drawer" />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    drawer: false,
+  }),
+  methods: {
+    newGame() {
+      if (confirm("Are you sure you want to start a new game?")) {
+        localStorage.clear();
+        window.location.reload();
+      }
+    },
+  },
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
